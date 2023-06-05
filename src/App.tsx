d098ec6349f638ec6176cab9e7d62ef6e3e4090a
smartdev0612 <danieldev0612@gmail.com>
2023-06-04 19:45:10 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { createServer, Response } from 'miragejs'
+import data from './data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+createServer({
+  routes() {
+    this.get('/api/users', () => {
+      return new Response(401, {}, { errors: ['You are in the wrong place'] })
+    })
+
+    this.get('/api/users/:id', (schema, request) => {
+      const id = parseInt(request.params.id)
+      return [data.find((data) => data.id === id)]
+    })
+
+    this.get('/api/search', (schema, request) => {
+      const query = request.queryParams['query']
+      if (query === 'red') {
+        return [{ id: 1, name: 'red' }]
+      } else {
+        return [{ id: 1, name: 'black' }]
+      }
+    })
+  },
+})
+
+type User = {
+  id: number
+  name: string
 }
 
-export default App;
+function App() {
+  let [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch(`/api/users`)
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+  }, [])
+
+  return (
+    <div className="App">
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
